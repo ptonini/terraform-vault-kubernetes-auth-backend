07b5ca3157b9a1a21d9823d0d3211f558e467d0b
ptonini <pedro.tonini@hotmail.com>
2023-12-17 22:36:11 -0300

    feat: added roles submodule

--- main.tf
+++ main.tf
@@ -11,3 +11,14 @@ resource "vault_kubernetes_auth_backend_config" "this" {
   pem_keys               = var.pem_keys
   disable_iss_validation = var.disable_iss_validation
 }
+
+module "roles" {
+  source                           = "ptonini/kubernetes-auth-role/vault"
+  version                          = "~> 1.1.1"
+  for_each                         = var.roles
+  name                             = each.key
+  backend                          = vault_auth_backend.this.path
+  bound_service_account_names      = coalesce(each.value.bound_service_account_names, [each.key])
+  bound_service_account_namespaces = each.value.bound_service_account_namespaces
+  policy_definitions               = each.value.policy_definitions
+}
--- variables.tf
+++ variables.tf
@@ -13,10 +13,23 @@ variable "kubernetes_ca_cert" {}
 variable "token_reviewer_jwt" {}
 
 variable "pem_keys" {
-  type = set(string)
+  type    = set(string)
   default = []
 }
 
 variable "disable_iss_validation" {
   default = true
+}
+
+variable "roles" {
+  type = map(object({
+    name                             = optional(string)
+    audience                         = optional(string)
+    token_ttl                        = optional(number)
+    token_max_ttl                    = optional(number)
+    bound_service_account_names      = optional(set(string))
+    bound_service_account_namespaces = optional(set(string))
+    policies                         = optional(list(string))
+    policy_definitions               = optional(list(string))
+  }))
 }
